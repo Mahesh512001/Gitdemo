cde35915b412f0f961c3753c2f6f117fc6aec62e
Mahesh512001 <chaurasiyamahesh012345@gmail.com>
2025-08-29 11:44:00 +0530

    29_08_25 pattern programming

---
+++ C_Language/29_08_25.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+  int rn;
+  int cn;
+  printf("Enter the number First Cn and Rn : ");
+  scanf("%d%d", &cn, &rn);
+  for (int i = 1; i <= rn/2; i++)
+  {
+    
+    for (int c = 1; c <= cn; c++)
+    {
+      if (i % 2 == 0)
+      {
+        printf("%c", 96 + i);
+      }
+      else
+        printf("%c", 64 + i);
+    }
+     printf("\n");
+    
+     for (int c = 1; c <= cn; c++)
+    {
+      if (i % 2 == 0)
+      {
+         printf("%c", 64 + i);
+      
+      }
+      else   printf("%c", 96 + i);
+       
+    }
+    printf("\n");
+  }
+}
